4b9d15241966ab0ae80db3bc98c9471d5f524d14
Ledesmaxz <ledesmazja@gmail.com>
2024-08-21 22:19:02 -0500

    Create logo, search bar and shop car in header

--- test-react-git/src/components/Header.tsx
+++ test-react-git/src/components/Header.tsx
@@ -1,10 +1,18 @@
 import '../styles/Header.css'
 
+import Logo from '../assets/logo.png';
+import carrito from '../assets/preview.png'
+import search from '../assets/search.png'
 
 export default function Header() {
   return (
-    <header className='header'> 
-      Header for course
+    <header className='header'>
+        <img src={Logo} alt='Logo' className='Logo'/> 
+        <p className='header__se'>
+            <input type="Busqueda" className='header__search'/>
+            <img src={search} alt='Search' className='Search'/>
+        </p>
+        <img src={carrito} alt='Carrito' className='Logo'/> 
     </header>
   )
 }
