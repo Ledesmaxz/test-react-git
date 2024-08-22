fea1f727d7a59b0ae571232cc70d57dcf7cdd292
Ledesmaxz <ledesmazja@gmail.com>
2024-08-21 22:55:43 -0500

    Create first schema for categories

---
+++ test-react-git/src/components/Categories.tsx
@@ -0,0 +1,13 @@
+import '../styles/Categories.css'
+
+export const Categories = () => {
+  return (
+    <div className="categories">
+        <p>Mascotas</p>
+        <p>Alimentos  y Comida</p>
+        <p>Accesorios</p>
+        <p>Cuidado y salud</p>
+        <p>Ofertas</p>
+    </div>
+  )
+}
--- test-react-git/src/components/Main.tsx
+++ test-react-git/src/components/Main.tsx
@@ -1,9 +1,13 @@
 import '../styles/Main.css'
 
+import { Categories } from './Categories';
+
 export default function Main() {
   return (
     <main className='main'>
-      <aside className='main__aside'>categories</aside>
+      <aside className='main__aside'>
+        <Categories />
+      </aside>
       <section className='main__products'>
         products
       </section>
