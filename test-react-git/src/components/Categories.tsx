cf8731b2811b27000326fec3af88a8aaa2f37a32
Ledesmaxz <ledesmazja@gmail.com>
2024-08-26 18:52:32 -0500

    Repair categories, footer and font

--- test-react-git/src/components/Categories.tsx
+++ test-react-git/src/components/Categories.tsx
@@ -1,13 +1,29 @@
-import '../styles/Categories.css'
+import React, { useState } from 'react';
+import '../styles/Categories.css';
+import desplegableIcon from '../assets/desplegable.png'; 
 
 export const Categories = () => {
+  const [activeIndex, setActiveIndex] = useState<number | null>(null);
+
+  const handleToggle = (index: number) => {
+    setActiveIndex(activeIndex === index ? null : index);
+  };
+
   return (
     <div className="categories">
-        <p>Mascotas</p>
-        <p>Alimentos  y Comida</p>
-        <p>Accesorios</p>
-        <p>Cuidado y salud</p>
-        <p>Ofertas</p>
+      {['Mascotas', 'Alimentos', 'Accesorios', 'Salud', 'Ofertas'].map((category, index) => (
+        <div key={index}>
+          <p onClick={() => handleToggle(index)}>
+            {category} <img src={desplegableIcon} alt="Desplegar menú" className="arrow-icon" />
+          </p>
+          {activeIndex === index && (
+            <div className="submenu">
+              <p>Opción 1</p>
+              <p>Opción 2</p>
+            </div>
+          )}
+        </div>
+      ))}
     </div>
-  )
-}
+  );
+};
